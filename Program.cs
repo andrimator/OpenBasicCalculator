5891ecc3bcdb74b5a157153d562e612c30624e4f
BuildTools <unconfigured@null.spigotmc.org>
2021-06-14 09:06:57 -0500

    Codigo TOTALMENTE reescrito. Misma funcionalidad.

--- Program.cs
+++ Program.cs
@@ -10,164 +10,134 @@ namespace InteractiveConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.Title = "Open Basic Calculator v0.1-alpha";
-            Console.ForegroundColor = ConsoleColor.White;
-
-            Console.WriteLine("================================");
-            Console.WriteLine("Open Basic Calculator 0.1");
-            Console.WriteLine("================================");
-            Console.WriteLine("Created by andrimator\n");
-
+            Title();
             ModeSelect();
+        }
+
+        static void Title()
+        {
+            Console.Title = "Open Basic Calculator v0.2-alpha";
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("================================");
+            Console.WriteLine("Open Basic Calculator 0.2-alpha");
+            Console.WriteLine("================================");
+            Console.WriteLine("Created by andrimator");
         }
 
         static void ModeSelect()
         {
-            Console.Write("Seleccione el modo a utilizar (+, -, *, /) : ");
+            Console.Write("\nSeleccione el modo a utilizar (+, -, *, /) : ");
             string calcMode = Convert.ToString(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.White;
             switch(calcMode)
             {
                 case "+":
-                    ActivateSumaMode();
+                    Console.WriteLine("\n[INFO] Modo de Suma activada\nFormato: a + b + c (only numbers)");
+                    ObtenerVariables("+");
                     break;
                 case "-":
-                    ActivateRestaMode();
+                    Console.WriteLine("\n[INFO] Modo de Resta activada\nFormato: a - b - c (only numbers)");
+                    ObtenerVariables("-");
                     break;
                 case "*":
-                    ActivateMultiMode();
+                    Console.WriteLine("\n[INFO] Modo de Multiplicacion activada\nFormato: a * b * c (only numbers)");
+                    ObtenerVariables("*");
                     break;
                 case "/":
-                    ActivateDivMode();
+                    Console.WriteLine("\n[INFO] Modo de Division activada\nFormato: a / b / c (only numbers)");
+                    ObtenerVariables("/");
                     break;
                 default:
-                    ModeSelectError();
+                    Console.ForegroundColor = ConsoleColor.Red; //Color Rojo
+                    Console.Write("Has insertado un modo no válido! ");
+                    ModeSelect();
                     break;
             }
             Console.ReadLine();
         }
 
-        static void ModeSelectError()
-        {
-            Console.ForegroundColor = ConsoleColor.Red; //Color Rojo
-            Console.Write("Has insertado un modo no válido! Selecciona el modo a utilizar (+, -, *, /) : ");
-            string calcMode = Convert.ToString(Console.ReadLine());
-            switch(calcMode)
-            {
-                case "+":
-                    ActivateSumaMode();
-                    break;
-                case "-":
-                    ActivateRestaMode();
-                    break;
-                case "*":
-                    ActivateMultiMode();
-                    break;
-                case "/":
-                    ActivateDivMode();
-                    break;
-                default:
-                    ModeSelectError();
-                    break;
-            }
-        }
-
-        static void ActivateSumaMode()
+        static void ObtenerVariables(string mode)
         {
             Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("\n[INFO] Modo de Suma activada\nFormato: a + b + c (only numbers)");
             Console.Write("Enter a number: ");
             double num1 = Convert.ToDouble(Console.ReadLine());
             Console.Write("Enter another number: ");
             double num2 = Convert.ToDouble(Console.ReadLine());
             Console.Write("Enter another number: ");
             double num3 = Convert.ToDouble(Console.ReadLine());
-            SumadorMode3(num1, num2, num3);
+            switch(mode)
+            {
+                case "+":
+                    OperatorMode3(num1, num2, num3, "+");
+                    break;
+                case "-":
+                    OperatorMode3(num1, num2, num3, "+");
+                    break;
+                case "*":
+                    OperatorMode3(num1, num2, num3, "*");
+                    break;
+                case "/":
+                    OperatorMode3(num1, num2, num3, "/");
+                    break;
+            }
         }
 
-        static void SumadorMode3(double sa, double sb, double sc)
+        static void OperatorMode3(double a, double b, double c, string mode)
         {
-            double sr = sa + sb + sc;
-            Console.WriteLine("Final Operation:");
-            Console.WriteLine(sa + "+" + sb + "+" + sc + "=" + sr);
-            Console.WriteLine("Raw Result: " + sr);
-            Console.WriteLine("");
+            double mathResult;
+            switch(mode)
+            {
+                case "+":
+                    mathResult = a + b + c;
+                    Console.WriteLine("Final Operation:");
+                    Console.WriteLine(a + "+" + b + "+" + c + "=" + mathResult);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Raw Result: " + mathResult);
+                    break;
+                case "-":
+                    mathResult = a - b - c;
+                    Console.WriteLine("Final Operation:");
+                    Console.WriteLine(a + "-" + b + "-" + c + "=" + mathResult);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Raw Result: " + mathResult);
+                    break;
+                case "*":
+                    mathResult = a * b * c;
+                    Console.WriteLine("Final Operation:");
+                    Console.WriteLine(a + "*" + b + "*" + c + "=" + mathResult);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Raw Result: " + mathResult);
+                    break;
+                case "/":
+                    mathResult = a / b / c;
+                    Console.WriteLine("Final Operation:");
+                    Console.WriteLine(a + "/" + b + "/" + c + "=" + mathResult);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Raw Result: " + mathResult);
+                    break;
+            }
+            Console.ForegroundColor = ConsoleColor.White;
             GivenResult();
         }
         
-        static void ActivateRestaMode()
-        {
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("\n[INFO] Modo de Resta activada\nFormato: a - b - c");
-            Console.Write("Enter a number: ");
-            double sum1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter another number: ");
-            double sum2 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter another number: ");
-            double sum3 = Convert.ToDouble(Console.ReadLine());
-            RestadorMode3(sum1, sum2, sum3);
-        }
-
-        static void RestadorMode3(double sa, double sb, double sc)
-        {
-            double sr = sa - sb - sc;
-            Console.WriteLine("Final Operation:");
-            Console.WriteLine(sa + "-" + sb + "-" + sc + "=" + sr);
-            Console.WriteLine("Raw Result: " + sr);
-            Console.WriteLine("");
-            GivenResult();
-        }
-
-        static void ActivateMultiMode()
-        {
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("\n[INFO] Modo de Multiplicacion activada\nFormato: a x b x c");
-            Console.Write("Enter a number: ");
-            double sum1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter another number: ");
-            double sum2 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter another number: ");
-            double sum3 = Convert.ToDouble(Console.ReadLine());
-            MultiplicatorMode3(sum1, sum2, sum3);
-        }
-
-        static void MultiplicatorMode3(double sa, double sb, double sc)
-        {
-            double sr = sa * sb * sc;
-            Console.WriteLine("Final Operation:");
-            Console.WriteLine(sa + "x" + sb + "x" + sc + "=" + sr);
-            Console.WriteLine("Raw Result: " + sr);
-            Console.WriteLine("");
-            GivenResult();
-        }
-        static void ActivateDivMode()
-        {
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("\n[INFO] Modo de Division activada\nFormato: a / b / c");
-            Console.Write("Enter a number: ");
-            double sum1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter another number: ");
-            double sum2 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter another number: ");
-            double sum3 = Convert.ToDouble(Console.ReadLine());
-            DividendoMode3(sum1, sum2, sum3);
-        }
-
-        static void DividendoMode3(double sa, double sb, double sc)
-        {
-            double sr = sa / sb / sc;
-            Console.WriteLine("Final Operation:");
-            Console.WriteLine(sa + "/" + sb + "/" + sc + "=" + sr);
-            Console.WriteLine("Raw Result: " + sr);
-            Console.WriteLine("");
-            GivenResult();
-        }
         static void GivenResult()
         {
             Console.Write("Do you wanna make another math operation? [Y/N]: ");
             string endTask = Console.ReadLine();
             endTask = endTask.ToUpper();
-            endTask = Convert.ToString(endTask);
-            if(endTask.Contains("Y")) ModeSelect(); else System.Environment.Exit(0);
+            if(endTask.Contains("Y"))
+            {
+                Console.Write("Clear previous math? [Y/N]: ");
+                string clearLog = Console.ReadLine();
+                clearLog = clearLog.ToUpper();
+                if(clearLog.Contains("Y"))
+                {
+                    Console.Clear();
+                    Title();
+                    ModeSelect();
+                }
+                else ModeSelect();
+            } else System.Environment.Exit(0);
         }
     }
 }
