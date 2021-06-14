ff76985b88def0787d4585c57cd6a31d1fffbbea
BuildTools <unconfigured@null.spigotmc.org>
2021-06-14 11:11:45 -0500

    Nuevo mensaje de error.

--- Program.cs
+++ Program.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 
 namespace InteractiveConsoleApp
 {
+    class globalVariables
+    {
+        public static int missCount= 0;
+        public static int missLimit= 3;
+    }
     class Program
     {
         static void Main(string[] args)
@@ -16,40 +21,46 @@ namespace InteractiveConsoleApp
 
         static void Title()
         {
-            Console.Title = "Open Basic Calculator v0.2-alpha";
+            Console.Title = "Open Basic Calculator v0.3-alpha";
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("================================");
-            Console.WriteLine("Open Basic Calculator 0.2-alpha");
+            Console.WriteLine("Open Basic Calculator 0.3-alpha");
             Console.WriteLine("================================");
-            Console.WriteLine("Created by andrimator");
+            Console.WriteLine("Created by andrimator - Made in C#");
         }
 
         static void ModeSelect()
         {
+            Console.ForegroundColor = ConsoleColor.White;
             Console.Write("\nSeleccione el modo a utilizar (+, -, *, /) : ");
             string calcMode = Convert.ToString(Console.ReadLine());
             Console.ForegroundColor = ConsoleColor.White;
             switch(calcMode)
             {
                 case "+":
-                    Console.WriteLine("\n[INFO] Modo de Suma activada\nFormato: a + b + c (only numbers)");
+                    Console.WriteLine("\nModo de Suma activada\nFormato: a + b + c (only numbers, otherwise it will crash)");
                     ObtenerVariables("+");
                     break;
                 case "-":
-                    Console.WriteLine("\n[INFO] Modo de Resta activada\nFormato: a - b - c (only numbers)");
+                    Console.WriteLine("\nModo de Resta activada\nFormato: a - b - c (only numbers, otherwise it will crash)");
                     ObtenerVariables("-");
                     break;
                 case "*":
-                    Console.WriteLine("\n[INFO] Modo de Multiplicacion activada\nFormato: a * b * c (only numbers)");
+                    Console.WriteLine("\nModo de Multiplicacion activada\nFormato: a * b * c (only numbers, otherwise it will crash)");
                     ObtenerVariables("*");
                     break;
                 case "/":
-                    Console.WriteLine("\n[INFO] Modo de Division activada\nFormato: a / b / c (only numbers)");
+                    Console.WriteLine("\nModo de Division activada\nFormato: a / b / c (only numbers, otherwise it will crash)");
                     ObtenerVariables("/");
                     break;
                 default:
                     Console.ForegroundColor = ConsoleColor.Red; //Color Rojo
-                    Console.Write("Has insertado un modo no válido! ");
+                    if(globalVariables.missCount >= globalVariables.missLimit) Console.Write("Has insertado un modo no válido!\nEscribe alguno de los caracteres contenidos en los parentesis! () ");
+                    else
+                    {
+                        globalVariables.missCount++;
+                        Console.Write("Has insertado un modo no válido!");
+                    }
                     ModeSelect();
                     break;
             }
@@ -58,26 +69,33 @@ namespace InteractiveConsoleApp
 
         static void ObtenerVariables(string mode)
         {
+            //int missCounter;
             Console.ForegroundColor = ConsoleColor.White;
-            Console.Write("Enter a number: ");
+            Console.Write("a. Enter a number: ");
             double num1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter another number: ");
+            Console.Write("b. Enter another number: ");
             double num2 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter another number: ");
+            Console.Write("c. Enter another number: ");
             double num3 = Convert.ToDouble(Console.ReadLine());
+            //Enviar variables a su respectivo operador
             switch(mode)
             {
                 case "+":
                     OperatorMode3(num1, num2, num3, "+");
                     break;
                 case "-":
-                    OperatorMode3(num1, num2, num3, "+");
+                    OperatorMode3(num1, num2, num3, "-");
                     break;
                 case "*":
                     OperatorMode3(num1, num2, num3, "*");
                     break;
                 case "/":
                     OperatorMode3(num1, num2, num3, "/");
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.Red; //Color Rojo
+                    //Console.Write("Invalid character! Try again, but this time with numbers. ");
+                    Console.Write("Invalid character! Try again. ");
                     break;
             }
         }
@@ -127,6 +145,7 @@ namespace InteractiveConsoleApp
             endTask = endTask.ToUpper();
             if(endTask.Contains("Y"))
             {
+                globalVariables.missCount = 0;
                 Console.Write("Clear previous math? [Y/N]: ");
                 string clearLog = Console.ReadLine();
                 clearLog = clearLog.ToUpper();
